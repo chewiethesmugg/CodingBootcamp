c0cf293ef10d094f1cd29c0499458fe38b98bc82
chewiethesmugg <46301510+chewiethesmugg@users.noreply.github.com>
2020-01-06 23:51:33 -0500

    Add files via upload

--- BasicProblem12.java
+++ BasicProblem12.java
@@ -1,5 +1,20 @@
 package BasicProblems;
 
 public class BasicProblem12 {
-
+	public int[] sumOfArrays(int[] arr1, int[] arr2) {
+		if(arr1==null&&arr2==null)return null;
+		if(arr1==null)return arr2;
+		if(arr2==null)return arr1;
+		int max=Math.max(arr1.length,arr2.length);
+		int min=Math.min(arr1.length, arr2.length);
+		int sum[]=new int[max];
+		for(int i=0;i<min;i++) {
+			sum[i]=arr1[i]+arr2[i];
+		}
+		for(int i=min;i<max;i++) {
+			if(arr1.length<arr2.length)sum[i]=arr2[i];
+		else {sum[i]=arr1[i];}
+		}
+		return sum;
+	}
 }
